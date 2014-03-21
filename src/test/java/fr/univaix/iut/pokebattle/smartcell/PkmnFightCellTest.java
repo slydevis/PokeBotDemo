deac14c622e50364bed65f7250f38e3c07505d34
Mouii <kevin.prudhomme555@gmail.com>
2014-03-21 17:22:38 +0100

    Début P11

---
+++ src/test/java/fr/univaix/iut/pokebattle/smartcell/PkmnFightCellTest.java
@@ -0,0 +1,19 @@
+package fr.univaix.iut.pokebattle.smartcell;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fr.univaix.iut.pokebattle.twitter.Tweet;
+
+public class PkmnFightCellTest {
+
+	PkmnFightCell cell = new PkmnFightCell();
+	
+	@Test
+	public void FightTest() {
+		assertEquals("", 
+				cell.ask(new Tweet("nedseb","@gantben #fight with @ronflex /cc @marclap")));
+	}
+	
+}
