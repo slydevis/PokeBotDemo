cd9f9bee17ebb5bbc6de8c080064eebc010baaea
Mouii <kevin.prudhomme555@gmail.com>
2014-04-04 16:49:58 +0200

    Maj P7-3

--- src/main/java/fr/univaix/iut/pokebattle/smartcell/OwnAskPkmnAtkCell.java
+++ src/main/java/fr/univaix/iut/pokebattle/smartcell/OwnAskPkmnAtkCell.java
@@ -1,12 +1,19 @@
 package fr.univaix.iut.pokebattle.smartcell;
 
+import fr.univaix.iut.pokebattle.jpa.Pokemon;
 import fr.univaix.iut.pokebattle.twitter.Tweet;
 
 public class OwnAskPkmnAtkCell implements SmartCell {
 
+    private Pokemon poke;
+    
 	public String decode(Tweet question) {
 		boolean hashA = false;
 		boolean aro = false;
+		if (!(question.getScreenName().equals(poke.getEleveur()))) {
+			return '@' + poke.getEleveur() + " is my owner";
+		}
+		
 		String text = question.getText(); //Cette variable récupère le contenu du tweet
 		int nbH = 0; //Variable concernant le nombre de HashTag
 		String att = ""; //Variable qui doit contenir #attack
@@ -39,16 +46,18 @@ public class OwnAskPkmnAtkCell implements SmartCell {
 				cible += text.charAt(i);
 			}
 		}
+		
 		return (cible + ' ' + att + ' ' + nomatt + '!' + ' ' + "/cc" + ' ' + '@'
 				+ question.getScreenName());
+
 	}
 
-
-
 	public String ask(Tweet question) {
-		if (question.getText().contains("#attack")) {
+        poke = PokeAskOwnerCell.recupInfo(question.getText());
+		if (poke != null && question.getText().contains("#attack")) {
             return decode(question);
-        } else {
+        } 
+		else {
             return null;
         }
 	}
